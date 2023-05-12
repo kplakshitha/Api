061798ba7efc1f415aebb5b4e8d677311a819e03
Lakshitha Gayan <lakshitha.gayan@nagarro.com>
2023-05-12 11:43:05 +0530

    Add project files.

---
+++ Controllers/CalculateController.cs
@@ -0,0 +1,18 @@
+﻿using Api.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CalculateController : ControllerBase
+    {
+        [Route("Add")]
+        [HttpPost]
+        public int Add(AddModel model)
+        {
+            return model.Number1 + model.Number2;
+        }
+    }
+}
---
+++ Model/AddModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Model
+{
+    public class AddModel
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+    }
+}
